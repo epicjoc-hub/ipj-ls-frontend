1efb88f8711281b1c12df5379876fe09e9f39e6a
epicjoc-hub <epicjoc@gmail.com>
2025-10-27 02:58:43 +0000

    Centered homepage layout and made Netlify-proof

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.mjs
+export default {
+  plugins: {
+    // Include plugin-urile pe care le folosești efectiv:
+    // 'postcss-import': {},   // dacă îl folosești
+    tailwindcss: {},           // dacă ai Tailwind
+    autoprefixer: {},
+  },
+};
